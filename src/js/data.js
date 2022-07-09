58d7a0849ebd64a0b37102e360d15d6f30c1db17
Maria Ferrari <maria.ferrari@kenzie.com.br>
2022-07-09 10:58:02 -0300

    ajustes css/html

---
+++ src/js/data.js
@@ -0,0 +1,17 @@
+let series = [
+    {
+        nome: "Nome 1",
+        sinopse: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+        imagemURL: "./src/assets/image.png"
+    },
+    {
+        nome: "Nome 2",
+        sinopse: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+        imagemURL: "./src/assets/image.png"
+    },
+    {
+        nome: "Nome 2",
+        sinopse: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
+        imagemURL: "./src/assets/image.png"
+    },
+]
